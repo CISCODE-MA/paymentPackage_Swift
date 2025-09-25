bc16f53dae9b5ba36d0211bfd488ca24c963c596
Zaiidmo <zaiidmoumnii@gmail.com>
2025-09-25 16:31:14 +0100

    ENV: Update the Package descriptive entry file and structuring the project

--- Package.swift
+++ Package.swift
@@ -5,20 +5,35 @@ import PackageDescription
 
 let package = Package(
     name: "PaymentPackage",
+    platforms: [
+        .iOS(.v14), .macOS(.v12),
+    ],
     products: [
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "PaymentPackage",
-            targets: ["PaymentPackage"]),
+            targets: ["PaymentPackage"]
+        ),
+        .library(name: "PaymentPackageUI", targets: ["PaymentPackageUI"]),
+
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "PaymentPackage"),
+            name: "PaymentPackage",
+            dependencies: [],
+            path: "Sources/PaymentPackage"
+        ),
+        .target(
+            name: "PaymentPackageUI",
+            dependencies: ["PaymentPackage"],
+            path: "Sources/PaymentPackageUI"
+        ),
         .testTarget(
             name: "PaymentPackageTests",
-            dependencies: ["PaymentPackage"]
+            dependencies: ["PaymentPackage"],
+            path: "Tests/PaymentPackageTests"
         ),
     ]
 )
